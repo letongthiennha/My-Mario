13fbcaa9a01f86bea34d252847b76ed4fd917247
Lê Tống Thiện Nhân <letongthiennha@gmail.com>
2025-05-16 19:29:34 +0700

    done

---
+++ build/CMakeFiles/Mario.dir/DependInfo.cmake
@@ -0,0 +1,76 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/Project/My Mario/src/Baddie.cpp" "CMakeFiles/Mario.dir/src/Baddie.cpp.obj" "gcc" "CMakeFiles/Mario.dir/src/Baddie.cpp.obj.d"
+  "D:/Project/My Mario/src/BanzaiBill.cpp" "CMakeFiles/Mario.dir/src/BanzaiBill.cpp.obj" "gcc" "CMakeFiles/Mario.dir/src/BanzaiBill.cpp.obj.d"
+  "D:/Project/My Mario/src/Block.cpp" "CMakeFiles/Mario.dir/src/Block.cpp.obj" "gcc" "CMakeFiles/Mario.dir/src/Block.cpp.obj.d"
+  "D:/Project/My Mario/src/BlueKoopaTroopa.cpp" "CMakeFiles/Mario.dir/src/BlueKoopaTroopa.cpp.obj" "gcc" "CMakeFiles/Mario.dir/src/BlueKoopaTroopa.cpp.obj.d"
+  "D:/Project/My Mario/src/BobOmb.cpp" "CMakeFiles/Mario.dir/src/BobOmb.cpp.obj" "gcc" "CMakeFiles/Mario.dir/src/BobOmb.cpp.obj.d"
+  "D:/Project/My Mario/src/BulletBill.cpp" "CMakeFiles/Mario.dir/src/BulletBill.cpp.obj" "gcc" "CMakeFiles/Mario.dir/src/BulletBill.cpp.obj.d"
+  "D:/Project/My Mario/src/BuzzyBeetle.cpp" "CMakeFiles/Mario.dir/src/BuzzyBeetle.cpp.obj" "gcc" "CMakeFiles/Mario.dir/src/BuzzyBeetle.cpp.obj.d"
+  "D:/Project/My Mario/src/CloudBlock.cpp" "CMakeFiles/Mario.dir/src/CloudBlock.cpp.obj" "gcc" "CMakeFiles/Mario.dir/src/CloudBlock.cpp.obj.d"
+  "D:/Project/My Mario/src/Coin.cpp" "CMakeFiles/Mario.dir/src/Coin.cpp.obj" "gcc" "CMakeFiles/Mario.dir/src/Coin.cpp.obj.d"
+  "D:/Project/My Mario/src/CollisionProbe.cpp" "CMakeFiles/Mario.dir/src/CollisionProbe.cpp.obj" "gcc" "CMakeFiles/Mario.dir/src/CollisionProbe.cpp.obj.d"
+  "D:/Project/My Mario/src/CourseClearToken.cpp" "CMakeFiles/Mario.dir/src/CourseClearToken.cpp.obj" "gcc" "CMakeFiles/Mario.dir/src/CourseClearToken.cpp.obj.d"
+  "D:/Project/My Mario/src/ExclamationBlock.cpp" "CMakeFiles/Mario.dir/src/ExclamationBlock.cpp.obj" "gcc" "CMakeFiles/Mario.dir/src/ExclamationBlock.cpp.obj.d"
+  "D:/Project/My Mario/src/EyesClosedBlock.cpp" "CMakeFiles/Mario.dir/src/EyesClosedBlock.cpp.obj" "gcc" "CMakeFiles/Mario.dir/src/EyesClosedBlock.cpp.obj.d"
+  "D:/Project/My Mario/src/EyesOpenedBlock.cpp" "CMakeFiles/Mario.dir/src/EyesOpenedBlock.cpp.obj" "gcc" "CMakeFiles/Mario.dir/src/EyesOpenedBlock.cpp.obj.d"
+  "D:/Project/My Mario/src/FireFlower.cpp" "CMakeFiles/Mario.dir/src/FireFlower.cpp.obj" "gcc" "CMakeFiles/Mario.dir/src/FireFlower.cpp.obj.d"
+  "D:/Project/My Mario/src/Fireball.cpp" "CMakeFiles/Mario.dir/src/Fireball.cpp.obj" "gcc" "CMakeFiles/Mario.dir/src/Fireball.cpp.obj.d"
+  "D:/Project/My Mario/src/FlyingGoomba.cpp" "CMakeFiles/Mario.dir/src/FlyingGoomba.cpp.obj" "gcc" "CMakeFiles/Mario.dir/src/FlyingGoomba.cpp.obj.d"
+  "D:/Project/My Mario/src/GameWindow.cpp" "CMakeFiles/Mario.dir/src/GameWindow.cpp.obj" "gcc" "CMakeFiles/Mario.dir/src/GameWindow.cpp.obj.d"
+  "D:/Project/My Mario/src/GameWorld.cpp" "CMakeFiles/Mario.dir/src/GameWorld.cpp.obj" "gcc" "CMakeFiles/Mario.dir/src/GameWorld.cpp.obj.d"
+  "D:/Project/My Mario/src/GlassBlock.cpp" "CMakeFiles/Mario.dir/src/GlassBlock.cpp.obj" "gcc" "CMakeFiles/Mario.dir/src/GlassBlock.cpp.obj.d"
+  "D:/Project/My Mario/src/Goomba.cpp" "CMakeFiles/Mario.dir/src/Goomba.cpp.obj" "gcc" "CMakeFiles/Mario.dir/src/Goomba.cpp.obj.d"
+  "D:/Project/My Mario/src/GreenKoopaTroopa.cpp" "CMakeFiles/Mario.dir/src/GreenKoopaTroopa.cpp.obj" "gcc" "CMakeFiles/Mario.dir/src/GreenKoopaTroopa.cpp.obj.d"
+  "D:/Project/My Mario/src/InvisibleBlock.cpp" "CMakeFiles/Mario.dir/src/InvisibleBlock.cpp.obj" "gcc" "CMakeFiles/Mario.dir/src/InvisibleBlock.cpp.obj.d"
+  "D:/Project/My Mario/src/Item.cpp" "CMakeFiles/Mario.dir/src/Item.cpp.obj" "gcc" "CMakeFiles/Mario.dir/src/Item.cpp.obj.d"
+  "D:/Project/My Mario/src/JumpingPiranhaPlant.cpp" "CMakeFiles/Mario.dir/src/JumpingPiranhaPlant.cpp.obj" "gcc" "CMakeFiles/Mario.dir/src/JumpingPiranhaPlant.cpp.obj.d"
+  "D:/Project/My Mario/src/Map.cpp" "CMakeFiles/Mario.dir/src/Map.cpp.obj" "gcc" "CMakeFiles/Mario.dir/src/Map.cpp.obj.d"
+  "D:/Project/My Mario/src/Mario.cpp" "CMakeFiles/Mario.dir/src/Mario.cpp.obj" "gcc" "CMakeFiles/Mario.dir/src/Mario.cpp.obj.d"
+  "D:/Project/My Mario/src/MessageBlock.cpp" "CMakeFiles/Mario.dir/src/MessageBlock.cpp.obj" "gcc" "CMakeFiles/Mario.dir/src/MessageBlock.cpp.obj.d"
+  "D:/Project/My Mario/src/MontyMole.cpp" "CMakeFiles/Mario.dir/src/MontyMole.cpp.obj" "gcc" "CMakeFiles/Mario.dir/src/MontyMole.cpp.obj.d"
+  "D:/Project/My Mario/src/MummyBeetle.cpp" "CMakeFiles/Mario.dir/src/MummyBeetle.cpp.obj" "gcc" "CMakeFiles/Mario.dir/src/MummyBeetle.cpp.obj.d"
+  "D:/Project/My Mario/src/Muncher.cpp" "CMakeFiles/Mario.dir/src/Muncher.cpp.obj" "gcc" "CMakeFiles/Mario.dir/src/Muncher.cpp.obj.d"
+  "D:/Project/My Mario/src/Mushroom.cpp" "CMakeFiles/Mario.dir/src/Mushroom.cpp.obj" "gcc" "CMakeFiles/Mario.dir/src/Mushroom.cpp.obj.d"
+  "D:/Project/My Mario/src/OneUpMushroom.cpp" "CMakeFiles/Mario.dir/src/OneUpMushroom.cpp.obj" "gcc" "CMakeFiles/Mario.dir/src/OneUpMushroom.cpp.obj.d"
+  "D:/Project/My Mario/src/PiranhaPlant.cpp" "CMakeFiles/Mario.dir/src/PiranhaPlant.cpp.obj" "gcc" "CMakeFiles/Mario.dir/src/PiranhaPlant.cpp.obj.d"
+  "D:/Project/My Mario/src/QuestionBlock.cpp" "CMakeFiles/Mario.dir/src/QuestionBlock.cpp.obj" "gcc" "CMakeFiles/Mario.dir/src/QuestionBlock.cpp.obj.d"
+  "D:/Project/My Mario/src/QuestionFireFlowerBlock.cpp" "CMakeFiles/Mario.dir/src/QuestionFireFlowerBlock.cpp.obj" "gcc" "CMakeFiles/Mario.dir/src/QuestionFireFlowerBlock.cpp.obj.d"
+  "D:/Project/My Mario/src/QuestionMushroomBlock.cpp" "CMakeFiles/Mario.dir/src/QuestionMushroomBlock.cpp.obj" "gcc" "CMakeFiles/Mario.dir/src/QuestionMushroomBlock.cpp.obj.d"
+  "D:/Project/My Mario/src/QuestionOneUpMushroomBlock.cpp" "CMakeFiles/Mario.dir/src/QuestionOneUpMushroomBlock.cpp.obj" "gcc" "CMakeFiles/Mario.dir/src/QuestionOneUpMushroomBlock.cpp.obj.d"
+  "D:/Project/My Mario/src/QuestionStarBlock.cpp" "CMakeFiles/Mario.dir/src/QuestionStarBlock.cpp.obj" "gcc" "CMakeFiles/Mario.dir/src/QuestionStarBlock.cpp.obj.d"
+  "D:/Project/My Mario/src/QuestionThreeUpMoonBlock.cpp" "CMakeFiles/Mario.dir/src/QuestionThreeUpMoonBlock.cpp.obj" "gcc" "CMakeFiles/Mario.dir/src/QuestionThreeUpMoonBlock.cpp.obj.d"
+  "D:/Project/My Mario/src/RedKoopaTroopa.cpp" "CMakeFiles/Mario.dir/src/RedKoopaTroopa.cpp.obj" "gcc" "CMakeFiles/Mario.dir/src/RedKoopaTroopa.cpp.obj.d"
+  "D:/Project/My Mario/src/ResourceManager.cpp" "CMakeFiles/Mario.dir/src/ResourceManager.cpp.obj" "gcc" "CMakeFiles/Mario.dir/src/ResourceManager.cpp.obj.d"
+  "D:/Project/My Mario/src/Rex.cpp" "CMakeFiles/Mario.dir/src/Rex.cpp.obj" "gcc" "CMakeFiles/Mario.dir/src/Rex.cpp.obj.d"
+  "D:/Project/My Mario/src/Sprite.cpp" "CMakeFiles/Mario.dir/src/Sprite.cpp.obj" "gcc" "CMakeFiles/Mario.dir/src/Sprite.cpp.obj.d"
+  "D:/Project/My Mario/src/Star.cpp" "CMakeFiles/Mario.dir/src/Star.cpp.obj" "gcc" "CMakeFiles/Mario.dir/src/Star.cpp.obj.d"
+  "D:/Project/My Mario/src/StoneBlock.cpp" "CMakeFiles/Mario.dir/src/StoneBlock.cpp.obj" "gcc" "CMakeFiles/Mario.dir/src/StoneBlock.cpp.obj.d"
+  "D:/Project/My Mario/src/Swooper.cpp" "CMakeFiles/Mario.dir/src/Swooper.cpp.obj" "gcc" "CMakeFiles/Mario.dir/src/Swooper.cpp.obj.d"
+  "D:/Project/My Mario/src/ThreeUpMoon.cpp" "CMakeFiles/Mario.dir/src/ThreeUpMoon.cpp.obj" "gcc" "CMakeFiles/Mario.dir/src/ThreeUpMoon.cpp.obj.d"
+  "D:/Project/My Mario/src/Tile.cpp" "CMakeFiles/Mario.dir/src/Tile.cpp.obj" "gcc" "CMakeFiles/Mario.dir/src/Tile.cpp.obj.d"
+  "D:/Project/My Mario/src/WoodBlock.cpp" "CMakeFiles/Mario.dir/src/WoodBlock.cpp.obj" "gcc" "CMakeFiles/Mario.dir/src/WoodBlock.cpp.obj.d"
+  "D:/Project/My Mario/src/YellowKoopaTroopa.cpp" "CMakeFiles/Mario.dir/src/YellowKoopaTroopa.cpp.obj" "gcc" "CMakeFiles/Mario.dir/src/YellowKoopaTroopa.cpp.obj.d"
+  "D:/Project/My Mario/src/YoshiCoin.cpp" "CMakeFiles/Mario.dir/src/YoshiCoin.cpp.obj" "gcc" "CMakeFiles/Mario.dir/src/YoshiCoin.cpp.obj.d"
+  "D:/Project/My Mario/src/main.cpp" "CMakeFiles/Mario.dir/src/main.cpp.obj" "gcc" "CMakeFiles/Mario.dir/src/main.cpp.obj.d"
+  "D:/Project/My Mario/src/utils.cpp" "CMakeFiles/Mario.dir/src/utils.cpp.obj" "gcc" "CMakeFiles/Mario.dir/src/utils.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/Mario.dir/src/CollisionProbe.cpp.obj.d
@@ -0,0 +1,12 @@
+CMakeFiles/Mario.dir/src/CollisionProbe.cpp.obj: \
+ D:\Project\My\ Mario\src\CollisionProbe.cpp \
+ D:/Project/MYMARI~1/inc/CollisionProbe.h \
+ D:/Project/MYMARI~1/inc/CollisionType.h \
+ D:/Project/MYMARI~1/inc/Drawable.h C:/raylib/raylib/src/raylib.h \
+ C:/msys64/ucrt64/lib/gcc/x86_64-w64-mingw32/14.2.0/include/stdarg.h \
+ C:/msys64/ucrt64/include/stdarg.h \
+ C:/msys64/ucrt64/include/_mingw_stdarg.h \
+ C:/msys64/ucrt64/include/vadefs.h C:/msys64/ucrt64/include/_mingw.h \
+ C:/msys64/ucrt64/include/_mingw_mac.h \
+ C:/msys64/ucrt64/include/_mingw_secapi.h \
+ C:/msys64/ucrt64/include/sdks/_mingw_ddk.h
